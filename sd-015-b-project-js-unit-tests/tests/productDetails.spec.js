39090c13947077272804f5ee91eda0b3fe5c6ad8
Camilla Del Guerso <camilladelguerso@gmail.com>
2021-09-29 14:00:53 -0300

    Adiciona projeto unit tests

---
+++ sd-015-b-project-js-unit-tests/tests/productDetails.spec.js
@@ -0,0 +1,49 @@
+const assert = require('assert');
+const productDetails = require('../src/productDetails');
+
+/*
+  Dadas duas strings que representam nomes de produtos, retorne um array contendo dois objetos com os detalhes dos respectivos produtos.
+
+  Parâmetros:
+  - Uma string;
+  - Uma string;
+
+  Comportamento:
+  productDetails('Alcool gel', 'Máscara') // Retorna:
+  [
+    {
+      name: 'Alcool gel'
+      details: {
+        productId: 'Alcool gel123'
+      }
+    },
+    {
+      name: 'Máscara'
+      details: {
+        productId: 'Máscara123'
+      }
+    }
+  ]
+
+  OBS: Lembre-se que você não precisa se preocupar com o describe e o it por enquanto, isso será aprendido posteriormente.
+*/
+
+describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
+  it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
+    // assert.fail();
+    // ESCREVA SEUS TESTES ABAIXO:
+    // Teste que o retorno da função é um array.
+    // Teste que o array retornado pela função contém dois itens dentro.
+    // Teste que os dois itens dentro do array retornado pela função são objetos.
+    // Teste que os dois objetos são diferentes entre si.
+    // Teste que os dois productIds terminam com 123.
+  });
+});
+
+assert.strictEqual(Array.isArray(productDetails('produto1', 'produto2')), true);
+assert.strictEqual(productDetails('produto1', 'produto2').length, 2);
+assert.strictEqual(typeof productDetails('produto1', 'produto2')[0], 'object');
+assert.strictEqual(typeof productDetails('produto1', 'produto2')[0], 'object');
+assert.notDeepStrictEqual(productDetails('produto1', 'produto2')[0], productDetails('produto1', 'produto2')[1]);
+assert.strictEqual(productDetails('produto1', 'produto2')[0].details.productId.slice(-3), '123');
+assert.strictEqual(productDetails('produto1', 'produto2')[1].details.productId.slice(-3), '123');
